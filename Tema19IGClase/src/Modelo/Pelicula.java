af831acbe837af798cdc6682e8efc7ace2398f72
sandralaz <slf0006@alu.medac.es>
2024-05-26 15:24:41 +0200

    modificación paquete modelo clase pelicula

--- Tema19IGClase/src/Modelo/Pelicula.java
+++ Tema19IGClase/src/Modelo/Pelicula.java
@@ -18,34 +18,6 @@ public class Pelicula {
         this.publicacion = publicacion;
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDirector() {
-        return director;
-    }
-
-    public void setDirector(String director) {
-        this.director = director;
-    }
-
-    public int getPublicacion() {
-        return publicacion;
-    }
-
-    public void setPublicacion(int publicacion) {
-        this.publicacion = publicacion;
-    }
-
-    @Override
-    public String toString() {
-        return "Pelicula{" + "titulo=" + titulo + ", director=" + director + ", publicacion=" + publicacion + '}';
-    }
     
     
 }
